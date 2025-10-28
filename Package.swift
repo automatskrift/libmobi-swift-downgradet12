97ff43765d77686c7aecc9464a9e52a98a5de8fa
automatskrift <automatskrift@gmail.com>
2025-10-28 21:39:25 +0100

    Change minimum iOS platform version to v12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "libmobi",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "libmobi",
